b55dada04145848e25915a59775994d09e2ea4c3
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-18 12:40:17 -0400

    Update getQuestionBank.php

--- Back/getQuestionBank.php
+++ Back/getQuestionBank.php
@@ -1,53 +1,67 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+      table, td, th {
+        border: 1px solid block;
+        padding: 5px;
+      }
+      th {
+        text-align: left;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+      error_reporting(E_ALL);
+      ini_set('display_errors', 1);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+      $dbhost = "sql1.njit.edu";
+      $dbuser = "hy276";
+      $dbpass = "HY9Co7Qkq";
+      $dbname = "hy276";
+      //login to
+      $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-$dbhost = "sql1.njit.edu";
-$dbuser = "hy276";
-$dbpass = "HY9Co7Qkq";
-$dbname = "hy276";
-//login to
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+      if (!$conn) {
+        die("Cannot connect to DB: " . mysqli_connect_error());
+      }
 
-if (!$conn) {
-  die("Cannot connect to DB: " . mysqli_connect_error());
-}
+      $query = "SELECT * FROM `question_bank`";
 
-$query = "SELECT DISTINCT `exam_name` FROM `exams`";
+      if ($response = mysqli_query($conn, $query)) {
+        echo "<table>
+              <tr>
+              <th>Question ID</th>
+              <th>Question Name</th>
+              <th>Question</th>
+              <th>Topic</th>
+              <th>Difficulty</th>
+              </tr>";
 
-$itWorked = false;
+        while ($row = mysqli_fetch_array($response)) {
+          echo "<tr>";
+          echo "<td>" . $row['question_id'] . "</td>";
+          echo "<td>" . $row['name'] . "</td>";
+          echo "<td>" . $row['question_text'] . "</td>";
+          echo "<td>" . $row['topic'] . "</td>";
+          echo "<td>" . $row['difficulty'] . "</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+      }
+      else {
+        die("query failed to db:");
+      }
 
-if ($response = mysqli_query($conn, $query)) {
-  echo "<table>
-  	    <tr>
-	      <th>Exam Name </th>
-	      </tr>";
+      mysqli_close($conn);
 
-  while ($row = mysqli_fetch_array($response)) {
-    echo "<tr>";
-    echo "<td>" . $row['exam_name'] . "</td>";
-    echo "<td> <button type='button' id='take_exam' onclick='selectExam()>Take this exam</button> </td>";
-    echo "</tr>";
-  }
-  echo "</table>";
-}
-else {
-  die("Query failed");
-}
-
-mysqli_close($conn);
-  
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
+     ?>
+  </body>
+</html>

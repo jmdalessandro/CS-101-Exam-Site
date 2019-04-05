5228f25252f0782469e0f2884f67cc87516d6fa9
joehudson <47796668+joehudson@users.noreply.github.com>
2019-04-05 15:38:49 -0400

    Update submitExam.php

--- Front/submitExam.php
+++ Front/submitExam.php
@@ -1,5 +1,24 @@
 <?php 
-
+//front
+$url = 'https://web.njit.edu/~hy276/beta/mid/submitExam.php';
 $data = file_get_contents("php://input");
 
-echo $data;
+$opts = [
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => 1,
+  CURLOPT_FOLLOWLOCATION => 1,
+  CURLOPT_POST => 1,
+  CURLOPT_POSTFIELDS => $data
+];
+
+$ch = curl_init();
+
+curl_setopt_array($ch, $opts);
+
+$result = curl_exec($ch);
+
+if ($result === false) {
+  die("curl didnt work");
+}
+
+echo $result;

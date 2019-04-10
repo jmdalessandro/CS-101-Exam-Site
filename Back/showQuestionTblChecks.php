b7efbba0a799206ba009ae60735382fa17a023c7
joehudson <47796668+joehudson@users.noreply.github.com>
2019-04-10 13:05:51 -0400

    Update showQuestionTblChecks.php

--- Back/showQuestionTblChecks.php
+++ Back/showQuestionTblChecks.php
@@ -6,7 +6,7 @@
 
       $dbhost = "sql1.njit.edu";
       $dbuser = "hy276";
-      $dbpass = "HY9Co7Qkq";
+      $dbpass = "FbRHBUeZ";
       $dbname = "hy276";
       //login to
       $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
@@ -16,30 +16,29 @@
       }
 
       $query = "SELECT * FROM `question_bank`";
-
+      $table = 'addQuestionTbl';
       if ($response = mysqli_query($conn, $query)) {
-        echo "<label for='exam_name'>Exam Name</label>";
-	echo "<input type='text' id='exam_name'><br>";
 	echo "<table id='addQuestionTbl'>
-              <tr>
-              <th>Check to add Question</th>
-	      <th>Score</th>
-              <th>Question ID</th>
-              <th>Question Name</th>
+        <tr>
+        <th>Check to add Question</th>
+        <th>Question ID</th>
+        <th>Question Name</th>
 	      <th>Question</th>
-              <th>Topic</th>
-              <th>Difficulty</th>
-              </tr>";
+        <th>Topic</th>
+        <th>Difficulty</th>
+        <th>Constraint</th>
+        </tr>";
 
         while ($row = mysqli_fetch_array($response)) {
           echo "<tr>";
-          echo "<td> <input type='checkbox' id='checkbox'> </td>";
-          echo "<td> <input type='text' id='points'> </td>";
-	  echo "<td id='qid'>" . $row['question_id'] . "</td>";
+          echo "<td> <input type='checkbox' id='checkbox' onchange='handleChk()'> </td>";
+          //echo "<td> <input type='text' id='points'> </td>";
+	        echo "<td id='qid'>" . $row['question_id'] . "</td>";
           echo "<td id='qname'>" . $row['name'] . "</td>";
-	  echo "<td id='qtext'>" . $row['question_text'] . "</td>";
+	        echo "<td id='qtext'>" . $row['question_text'] . "</td>";
           echo "<td id='top'>" . $row['topic'] . "</td>";
           echo "<td id='diff'>" . $row['difficulty'] . "</td>";
+          echo "<td id='constraint'>" . $row['constraint'] . "</td>";
           echo "</tr>";
 
         }

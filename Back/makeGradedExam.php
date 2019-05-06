478e66786f64355cce312fa2befb4aaa65a6fd84
joehudson <47796668+joehudson@users.noreply.github.com>
2019-05-06 19:01:50 -0400

    Update makeGradedExam.php

--- Back/makeGradedExam.php
+++ Back/makeGradedExam.php
@@ -3,9 +3,8 @@
 error_reporting(-1);
 //ini_set('display_errors',1);
 
-$data = file_get_contents("php://input");
+$data = trim(file_get_contents("php://input"), "\x0");
 $array = json_decode($data, true);
-
 $exam_name = $array['examName']; 
 
 //mysql credentials
@@ -22,14 +21,29 @@ if (!$conn) {
 
 $query = "SELECT * FROM `exam_list`,`student_exam`,`question_bank` WHERE `exam_list`.`exam_name` = `student_exam`.`exam_name`
           AND `exam_list`.`exam_name` = '$exam_name' AND `exam_list`.`question_id` = `question_bank`.`question_id`";
+$commentQuery = "SELECT * FROM `exam_comments`";
 $itWorked = false;
 $exam_answers = $exam_scores = $partials = $totalScore = $exam_comments = $exam_questions = [];
 $count = 0;
+$commentCount = 1;
+$questionCount = 1;
 
 if (!isset($json)) {
   $json = new stdClass();
 }
+if ($response = mysqli_query($conn, $commentQuery)) {
+  $itWorked = true;
+  while($row = mysqli_fetch_array($response)) {
+    $count++;
+    for ($questionCount = 1; $questionCount <= 3; $questionCount++) {
+      for ($commentCount = 1; $commentCount <= 5; $commentCount++) {
+        array_push($exam_comments, $row['q' . $questionCount . 'C' . $commentCount]);
+      }
+    }
+  }
+}
 
+$count = 0;
 if ($response = mysqli_query($conn, $query)) { //generate quiz from database
   $itWorked = true;
   $json->exam_name = $exam_name;
@@ -40,8 +54,7 @@ if ($response = mysqli_query($conn, $query)) { //generate quiz from database
     $exam_scores[$count] = $row['q' . $count . '_score'];
     $partials[$count] = $row['q' . $count . '_parts'];
     $totalScore[$count] = $row['score'];
-    $exam_comments = $row['exam_comment'];
-    //print_r($row);
+    //$exam_comments = $row['exam_comment']; 
   }
 }
 else {
@@ -55,7 +68,7 @@ if ($itWorked) {
   $json->exam_scores = $exam_scores;
   $json->partials = $partials;
   $json->totalScore = $totalScore;
-  $json->exam_comments = $exam_comments;
+  $json->exam_comments = $exam_comments; 
   $json->msg = "got graded exam";
 }
 else if ($itWorked === false) {
